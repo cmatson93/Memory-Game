2c12f01b0068e7057d0fe01498ac2e55e87aaa30
Christina Ng <cmatson93@gmail.com>
2019-02-11 18:56:09 -0800

    Updates

---
+++ precache-manifest.731a23b91dde014489b2c5b8ff16322a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "f71b7c7caccc2389bb9b",
+    "url": "/Memory-Game/static/js/main.f71b7c7c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/Memory-Game/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f71b7c7caccc2389bb9b",
+    "url": "/Memory-Game/static/css/main.2f9e254c.chunk.css"
+  },
+  {
+    "revision": "974073ad60f96c380cbf6ae5169d5694",
+    "url": "/Memory-Game/index.html"
+  }
+];
